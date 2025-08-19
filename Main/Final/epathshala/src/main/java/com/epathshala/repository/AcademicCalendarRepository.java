3d614bb64f5b4fa608e1cc8b1bf047ad0f309fe2
Saket M Kharche <108940657+saketkharche@users.noreply.github.com>
2025-08-19 16:57:27 +0530

    Signed-off-by: Saket M Kharche <108940657+saketkharche@users.noreply.github.com> commit

--- Main/Final/epathshala/src/main/java/com/epathshala/repository/AcademicCalendarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epathshala.repository;
-
-import com.epathshala.entity.AcademicCalendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcademicCalendarRepository extends JpaRepository<AcademicCalendar, Long> {
-}
